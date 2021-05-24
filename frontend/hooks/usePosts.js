9c04a1e6327e51cc5a30479b4e31a04d386f6e6e
Rafael-Batista-Dev <dev.rafaelbatista@gmail.com>
2021-05-24 10:01:13 -0300

    kay

--- frontend/hooks/usePosts.js
+++ frontend/hooks/usePosts.js
@@ -5,7 +5,7 @@ const usePosts = (posts) => {
   const Posts = () => (
     <>
       {posts.map(post => (
-        <a href="https://nextjs.org/docs" className={styles.card}>
+        <a href="https://nextjs.org/docs" className={styles.card} key={post.id}>
           <h2>{post.titulo} &rarr;</h2>
           <img src={`http://localhost:1337${post.foto.url}`} width={200} height={100} />
           <p>{post.descricao}</p>
