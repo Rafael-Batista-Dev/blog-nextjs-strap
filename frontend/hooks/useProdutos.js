ef4ce5468622ffac4a6417a75a4e86010f0da143
Rafael-Batista-Dev <dev.rafaelbatista@gmail.com>
2021-05-26 11:02:27 -0300

    date-fns

--- frontend/hooks/useProdutos.js
+++ frontend/hooks/useProdutos.js
@@ -2,6 +2,9 @@ import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import { fromImageToUrl, API_URL } from '../utils/urls'
 import { twoDecimals } from '../utils/format'
+import { formatDistance } from 'date-fns'
+import pt from 'date-fns/locale/pt'
+
 import axios from 'axios'
 
 
@@ -14,7 +17,9 @@ const useProdutos = (produtos) => {
           <a className={styles.card} target="_blank">
             <div><h4 style={{ textAlign: "justify" }}>{produto.nome}</h4></div>
             <div style={{ padding: " 20px 0", textAlign: "center" }} ><img src={fromImageToUrl(produto.foto)} width={200} height={200} /></div>
-            <div style={{ textAlign: "left" }}><h4>Publicado: {produto.published_at}</h4></div>
+            <div style={{ textAlign: "left" }}><h4>Há: {formatDistance(new Date(produto.published_at), new Date(), {
+              locale: pt
+            })}</h4></div>
             <div style={{ textAlign: "left" }}><h4>R$: {twoDecimals(produto.preco)}</h4></div>
             <div><h4>Categoria: {produto.categoria_pro.nome_categoria_pro}</h4></div>
           </a>
